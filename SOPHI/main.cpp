6dcc62b3fd00b371f391c00e7fdc73655308eeb4
Pierre lulu <luttmannpierre@gmail.com>
2023-10-28 12:24:56 +0200

    Dll dependencies added, architecture complete

--- SOPHI/main.cpp
+++ SOPHI/main.cpp
@@ -1,6 +1,11 @@
 //main here ?
+#include<iostream>
+#include"ImageNdg.h"
 
 int main()
 {
+	CImageNdg img("chro.bmp");
+	img.transformation("complement").seuillage("otsu").sauvegarde();
+
 	return 0;
 }
